5aec74c30f8bc340616d3d1ff1361ace6f1202f9
DanFein <danfeinstat@gmail.com>
2019-12-01 16:54:47 -0800

    baseline seed script complete

--- database/database.js
+++ database/database.js
@@ -17,26 +17,50 @@ sequelize.authenticate()
         console.log('Unable to connect to the database:', err);
     });
 const Users = sequelize.define('users', {
-    firstName: Sequelize.STRING,
-    lastName: Sequelize.STRING,
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: Sequelize.STRING,
     friendsNumber: Sequelize.INTEGER,
     reviewsNumber: Sequelize.INTEGER
 });
 const Restaurants = sequelize.define('restaurants', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     name: Sequelize.STRING,
 });
 const Reviews = sequelize.define('reviews', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     body: Sequelize.STRING,
     stars: Sequelize.INTEGER,
     userId: Sequelize.STRING,
     dishId: Sequelize.STRING
 })
 const Dishes = sequelize.define('dishes', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     name: Sequelize.STRING,
     price: Sequelize.DECIMAL,
     restuarantId: Sequelize.STRING
 })
 const Images = sequelize.define('images', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     source: Sequelize.STRING,
     caption: Sequelize.STRING,
     dishId: Sequelize.STRING
@@ -53,5 +77,5 @@ module.exports = {
     Restaurants,
     Reviews,
     Dishes,
-    Images
+    Images,
 }
--- database/seed.js
+++ database/seed.js
@@ -1,48 +1,74 @@
 const { Users, Restaurants, Reviews, Images, Dishes } = require('./database');
 const faker = require('faker');
-// const makeDollarAmount = () => {
-//     return Number((Math.random() * 1000).toFixed(2))
-// };
 
-// const makeStockAmount = () => {
-//     return Math.floor(Math.random() * 100)
-// }
-
-// const makeTickerSymbol = () => {
-//     let ticker = '';
-//     let size = Math.floor(Math.random() * (5-1) + 1);
-//     let randNum = () => {
-//         return Math.floor(Math.random() * (91-65) + 65)  
-//     }; 
-
-//     while (ticker.length < size) {
-//         ticker += String.fromCharCode(randNum()) 
-//     }
-
-//     return ticker;
-// }
 const dataGenerators = {
-    makeUser: function () {
-        const newUser = {
+    makeUserEntry: function () {
+        const newUserEntry = {
             name: faker.name.findName(),
             friendsNumber: Math.floor(Math.random() * 101),
-            reviewsnumber: Math.floor(Math.random() * 101)
+            reviewsNumber: Math.floor(Math.random() * 101)
         }
-        return newUser;
+        return newUserEntry;
     },
-    // makeReview: function() {
-    //     const newReview = {
-
-    //     }
-    // }
+    makeReviewEntry: function () {
+        const newReviewEntry = {
+            body: faker.lorem.paragraph(),
+            stars: Math.floor((Math.random() * 5) + 1),
+            userId: Math.floor((Math.random() * 50) + 1),
+            dishId: Math.floor((Math.random() * 20) + 1)
+        }
+        return newReviewEntry;
+    },
+    makeDishEntry: function () {
+        const newDishEntry = {
+            name: faker.lorem.words(),
+            price: Math.floor(Math.random() * 40 + 7) + Math.random(),
+            restaurantId: Math.floor(Math.random() * 100 + 1)
+        }
+        return newDishEntry;
+    },
+    makeImageEntry: function () {
+        const newImageEntry = {
+            source: `https://source.unsplash.com/random?food`,
+            caption: faker.lorem.sentence(),
+            dishId: Math.floor((Math.random() * 20) + 1)
+        }
+        console.log(newImageEntry);
+        return newImageEntry;
+    },
+    makeRestaurantEntry: function () {
+        const newRestaurant = {
+            name: faker.lorem.words()
+        }
+        return newRestaurant;
+    }
 }
-    // const seedQueries = {
-    //     seedUser: function (userQuantity) {
 
-    //     }
+const seedTable = function (entryNumber, makeFunction, table) {
+    let unsproutedSeeds = [];
+    for (let i = 0; i < entryNumber; i++) {
+        let dataObject = makeFunction();
+        unsproutedSeeds.push(
+            table.create(dataObject)
+        )
+    }
+    Promise.all(unsproutedSeeds)
+        .then((data) => {
+            console.log(`successfully updated table: ${table} with the following: ${data}`)
+        })
+        .catch((err) => {
+            console.log(`Error while seeding data: ${err}`)
+        })
+}
 
-    // }
+seedTable(100, dataGenerators.makeRestaurantEntry, Restaurants);
+seedTable(50, dataGenerators.makeUserEntry, Users);
+seedTable(100, dataGenerators.makeReviewEntry, Reviews);
+seedTable(20, dataGenerators.makeDishEntry, Dishes);
+seedTable(50, dataGenerators.makeImageEntry, Images);
 
-    // `https://source.unsplash.com/random?food`
+
+
+
 
 
