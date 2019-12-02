551569f456c7c7d54feef1d4f8d1920161a956ce
elchangtw <55995360+elchangtw@users.noreply.github.com>
2019-12-01 20:26:07 -0800

    Revert "added db test via api"

--- routes/routes.js
+++
@@ -1,7 +0,0 @@
-const controller = require('../controller/controller');
-const express = require('express');
-const router = express.Router();
-
-router.get('/users/ten', controller.users.getTen);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const port = 3000;
-const router = require('./routes/routes');
+let port = 3000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/client/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use('/api', router);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
