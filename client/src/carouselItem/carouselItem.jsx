5921ac9190e9f203b8d11255b4c1907ceaa84c8b
DanFein <danfeinstat@gmail.com>
2019-12-03 18:56:18 -0800

    css and basic structure of carouselItem component completed

--- client/src/carouselItem/carouselItem.jsx
+++ client/src/carouselItem/carouselItem.jsx
@@ -6,14 +6,14 @@ const CarouselItem = ({ imageUrl, price, name, photoNumber, reviewNumber }) => {
     return (
         <div className={styles.container}>
             <div className={styles.foodImage} style={{ backgroundImage: `url(${imageUrl})` }} >
-                <div className={styles.price}>{price}</div>
+                <div className={styles.price}>${price}</div>
             </div>
             {/* container for content */}
-            <div>
-                <h3>{name}</h3>
+            <div className={styles.contentContainer}>
+                <h3 className={styles.name}>{name}</h3>
                 {/* container for photoNumber and reviewsNumber, probably use flexbox for this */}
-                <div>
-                    <div>{photoNumber}</div><ul>{reviewNumber}</ul>
+                <div className={styles.flexRow}>
+                    <div className={styles.photoNumber}>{photoNumber} Photos </div><div className={styles.reviewNumber}> • {reviewNumber} Reviews</div>
                 </div>
             </div>
         </div >
