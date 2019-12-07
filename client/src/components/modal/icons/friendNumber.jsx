6593020abe2543b87951e54fe0e04cdd1a928d6e
DanFein <danfeinstat@gmail.com>
2019-12-06 16:47:46 -0800

    modalReviewComponent complete, need slight css tweeks on star review

--- client/src/components/modal/icons/friendNumber.jsx
+++ client/src/components/modal/icons/friendNumber.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './modalReviewIcons.module.css';
 
 const FriendNumber = props => (
-    <svg width={18} height={18} className="prefix__icon_svg" {...props}>
+    <svg width={18} height={18} {...props}>
         <path d="M7.904 9.43l-2.098 4.697a.9.9 0 01-1.612 0L2.096 9.43a.902.902 0 01.806-1.305h4.196c.67 0 1.105.705.806 1.305zM5 7.375a2 2 0 110-4 2 2 0 010 4z" />
         <path
             d="M15.904 9.43l-2.098 4.697a.89.89 0 01-.806.498.89.89 0 01-.806-.498L10.096 9.43a.902.902 0 01.806-1.305h4.195c.67 0 1.106.705.807 1.305zM13 7.375a2 2 0 110-4 2 2 0 010 4z"
--- client/src/components/modal/modalReview.jsx
+++ client/src/components/modal/modalReview.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import FriendNumber from './icons/friendNumber.jsx';
 import reviewNumber from './icons/reviewNumber.jsx';
 import styles from './modalReview.module.css';
@@ -25,24 +26,30 @@ class ModalReview extends Component {
         } else {
             formattedName = 'Anonymous';
         }
+        let starsArray = [0, 1, 2, 3, 4];
 
         return (
             <div className={styles.container}>
                 <img src={userAvatar} className={styles.avatar} />
                 <div className={styles.name}>{formattedName}</div>
-                <div>
-                    <div>
-                        <FriendNumber /> {friendNumber}
-                    </div>
-                    <div>
-                        <ReviewNumber /> {reviewNumber}
-                    </div>
+                <div className={styles.friends}>
+                    <FriendNumber /> {friendNumber}
                 </div>
-                <div>
-                    <div>{stars}</div>
-                    <div>{date}</div>
+                <div className={styles.reviews}>
+                    <ReviewNumber fill='rgb(241, 92, 0)' /> {reviewNumber}
                 </div>
-                <div>
+                <div className={styles.stars}>stars: {starsArray.map((data, index) => {
+                    return (
+                        <ReviewNumber fill={stars === 5 ? 'rgb(211,35,34)'
+                            : stars === 4 && index + 1 <= stars ? 'rgb(241,92,79)'
+                                : stars === 3 && index + 1 <= stars ? 'rgb(255,146,66)'
+                                    : stars === 2 && index + 1 <= stars ? 'rgb(254,192,17)'
+                                        : stars === 1 && index + 1 <= stars ? 'rgb(241,191,125)'
+                                            : 'rgb(204,204,204)'} />
+                    )
+                })}</div>
+                <div className={styles.date}>{moment(date).format("M/D/YYYY")}</div>
+                <div className={styles.body}>
                     {this.state.displayFull ? body : snippet}
                 </div>
                 <div className={styles.bodyToggle} onClick={this.toggleBody}>{this.state.displayFull ? 'Read less' : 'Read more'}</div>
