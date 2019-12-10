a9acd13a83ded4d6dc80e3c5775d0520dd206ad3
DanFein <danfeinstat@gmail.com>
2019-12-10 09:26:25 -0800

    updated port

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const port = 3000;
-const router = require('./routes/routes');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const port = 3002;
+const router = require("./routes/routes");
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/client/public')));
+app.use(express.static(path.join(__dirname, "/client/public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api', router);
+app.use("/api", router);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
